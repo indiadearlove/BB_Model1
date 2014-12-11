07a997931ad7d4d3f4e7fa03bda6f172949d2b85
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-11 16:11:14 +0000

    fix bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -13,4 +13,12 @@ describe Bike do
     expect(the_bike.broken?).to eq true
   end
 
+  it "should be able to be fixed" do
+    the_bike = Bike.new
+    the_bike.break!
+    expect(the_bike.broken?).to eq true
+    the_bike.fix!
+    expect(the_bike.broken?).to eq false
+  end
+
 end
