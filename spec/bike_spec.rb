9cf9df787c70fabb38bbc2f1eb9aa0a406967da7
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-11 16:05:58 +0000

    be able to break bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,7 +4,13 @@ describe Bike do
   
   it 'should not be broken after we create it' do 
     the_bike = Bike.new
-    expect(the_bike.broken?).to eq nil
+    expect(the_bike.broken?).to eq false
+  end
+
+  it 'should be able to break' do
+    the_bike = Bike.new
+    the_bike.break!
+    expect(the_bike.broken?).to eq true
   end
 
 end
